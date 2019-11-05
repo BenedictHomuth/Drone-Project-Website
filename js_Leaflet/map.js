368ab7e3604a74c1df7885d7387779fea29f4936
BenedictHomuth <benedict.homuth@gmail.com>
2019-11-05 21:42:56 +0100

    changes to map

--- js_Leaflet/map.js
+++ js_Leaflet/map.js
@@ -1,33 +1,3 @@
-//Here we geolocate the user
-if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(async position => {
-        const lat = position.coords.latitude;
-        document.getElementById('latitude').textContent = lat;
-        const lon = position.coords.longitude;
-        document.getElementById('longitude').textContent = lon;
-
-        //Here I create an object which then is send back to the server.
-        const data = { lat, lon };
-
-        createMap(lat,lon);
-
-        //This is an object that holds information about the data and structures of the package
-        //More on that: https://developer.mozilla.org/de/docs/Web/API/Fetch_API/Using_Fetch
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        };
-        const response = await fetch('/api', options);
-        const backMsg = await response.json();
-        console.log(backMsg);
-    });
-} else {
-    console.log('geolocation not available');
-} 
-
 //Leaflet Map implementation
 function createMap(lat, lon){
     const mymap = L.map('leafletMap').setView([lat, lon], 13);
@@ -38,3 +8,7 @@ function createMap(lat, lon){
     const myMarker = L.marker([lat,lon]).addTo(mymap);
     myMarker.bindTooltip("You're here!").openTooltip();
 }
+
+const lat = 51.509865;
+const lon = -0.118092;
+createMap(lat,lon);
